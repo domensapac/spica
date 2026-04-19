1fee77e5379e663ee3220e8d3e4cdf6195871e60
dddomennn <domen.sapac420@gmail.com>
2026-04-19 17:26:47 +0200

    Working on Users component

--- src/app/components/users/user.ts
+++ src/app/components/users/user.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, signal, OnInit, computed } from '@angular/core';
 import { UserService } from '../../services/user-service';
 
 @Component({
@@ -12,6 +12,16 @@ export class UserComponent {
 
   userList = signal<any[]>([]); 
   
+  currentPage = signal(1);
+  pageSize = signal(20); 
+
+  
+  ngOnInit(): void {
+    if(this.userList().length === 0){
+      this.getUsers();
+    }
+  }
+
   getUsers(){
     this.users.getUsers().subscribe({
       next: (res : any) => {
@@ -23,5 +33,19 @@ export class UserComponent {
         console.log("Error"); 
       }
     })
+  }
+
+  paginatedUsers = computed(() => {
+    const startIndex = (this.currentPage() - 1) * this.pageSize(); 
+    const endIndex = startIndex + this.pageSize(); 
+    return this.userList().slice(startIndex, endIndex); 
+  })
+
+  goToPage(page: number){
+    this.currentPage.set(page);
+  }
+
+  copyToClipboard(text: string){
+    navigator.clipboard.writeText(text);
   }
 }
--- src/app/services/user-service.ts
+++ src/app/services/user-service.ts
@@ -1,22 +1,43 @@
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, PLATFORM_ID, signal } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http'; 
 import { tap } from 'rxjs/operators';
+import { of } from 'rxjs';
+import { isPlatformBrowser } from '@angular/common'; 
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
+  private platformId = inject(PLATFORM_ID);
+
   private http = inject(HttpClient); 
 
   private userUrl = '/main-api/api/v1/users';
 
-  getUsers(){
-    const token = localStorage.getItem('token');
-    const headers = new HttpHeaders({
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${token}`
-    });
+  private cachedUsers = signal<any | null>(null); 
 
-    return this.http.get<any>(this.userUrl, { headers });
+  getUsers(){
+    if(isPlatformBrowser(this.platformId)){
+      if(this.cachedUsers() !== null){
+        return of(this.cachedUsers());
+      }
+      
+      const token = localStorage.getItem('token');
+      const headers = new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      });
+
+      return this.http.get<any>(this.userUrl, { headers }).pipe(tap(users => this.cachedUsers.set(users)));
+    }
+    else{
+      return of([]);
+    }
   }
+
+  clearCache(){
+    this.cachedUsers.set(null);
+  }
+
+
 }
