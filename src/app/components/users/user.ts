d43aaf9771be780991f76e2a5db87ba79bd08336
domensapac <domen.sapac10@gmail.com>
2026-04-19 19:25:44 +0200

    Angular Material for Users table

--- src/app/components/users/user.ts
+++ src/app/components/users/user.ts
@@ -1,25 +1,40 @@
-import { Component, inject, signal, OnInit, computed } from '@angular/core';
+import { Component, inject, signal, OnInit, computed, ViewChild } from '@angular/core';
 import { UserService } from '../../services/user-service';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
+import {MatSort, MatSortModule, Sort} from '@angular/material/sort';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
+
 
 @Component({
   selector: 'app-usercomponent',
-  imports: [],
+  imports: [MatTableModule, MatPaginatorModule, MatSortModule],
   templateUrl: './user.html',
   styleUrl: './user.css',
 })
 export class UserComponent {
   users = inject(UserService); 
+  private _liveAnnouncer = inject(LiveAnnouncer);
 
   userList = signal<any[]>([]); 
   
   currentPage = signal(1);
   pageSize = signal(20); 
+  displayedColumns: string[] = ['FirstName', 'LastName', 'Id', 'Email', 'changes'];
+  dataSource = new MatTableDataSource<any | null>([]);
 
-  
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
   ngOnInit(): void {
     if(this.userList().length === 0){
       this.getUsers();
     }
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
   }
 
   getUsers(){
@@ -27,7 +42,9 @@ export class UserComponent {
       next: (res : any) => {
         console.log("Success");
         console.log(res) ; 
-        this.userList.set(res as any[]); 
+        this.dataSource.data = res;
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
       },
       error: (err) =>{
         console.log("Error"); 
@@ -35,6 +52,15 @@ export class UserComponent {
     })
   }
 
+  announceSortChange(sortState: Sort) {
+    if (sortState.direction) {
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+    } else {
+      this._liveAnnouncer.announce('Sorting cleared');
+    }
+  }
+
+  /*
   paginatedUsers = computed(() => {
     const startIndex = (this.currentPage() - 1) * this.pageSize(); 
     const endIndex = startIndex + this.pageSize(); 
@@ -44,6 +70,7 @@ export class UserComponent {
   goToPage(page: number){
     this.currentPage.set(page);
   }
+  */
 
   copyToClipboard(text: string){
     navigator.clipboard.writeText(text);
